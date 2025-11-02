861a3475fc6c4b782a86c4cf6a8926590f6fec45
JezzyJesse <srstivenstrange@gmail.com>
2025-11-01 19:52:22 -0600

    Replace tags with technologies in Project component

--- src/common/components/sections/projects/_components/project.tsx
+++ src/common/components/sections/projects/_components/project.tsx
@@ -11,8 +11,7 @@ type ProjectProps = (typeof projectsData)[number];
 export default function Project({
   title,
   description,
-  tags,
-  imageUrl,
+  technologies,
   link,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
@@ -39,26 +38,19 @@ export default function Project({
           }
         >
           <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
-            <h3 className="text-2xl font-semibold uppercase"> {title}</h3>
+            <h3 className="text-2xl font-semibold uppercase">{title}</h3>
             <p className="mt-2 leading-relaxed">{description}</p>
             <ul className="mt-4 flex flex-wrap gap-2 sm:mt-auto">
-              {tags.map((tag, index) => (
+              {technologies.map((tech, index) => (
                 <li
                   className="rounded-full bg-[#ffcbb4] px-3 py-1 text-[0.7rem] uppercase tracking-wider dark:bg-[#ddbea9] dark:text-black"
                   key={index}
                 >
-                  {tag}
+                  {tech}
                 </li>
               ))}
             </ul>
           </div>
-
-          <Image
-            src={imageUrl}
-            alt="Project I worked on"
-            quality={95}
-            className="absolute -right-40 top-8 hidden w-[28.25rem] rounded-t-lg transition group-even:-left-40 group-even:right-[initial] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 sm:block"
-          />
         </section>
       </Link>
     </motion.div>
